b54e660869e119dc8946c8d452ecd09d13dd68aa
satya <satyabratsingh@gmail.com>
2018-06-02 12:52:39 +0100

    Adding list tricks

--- python/list/tricks/list_tricks.py
+++ python/list/tricks/list_tricks.py
@@ -1,8 +1,7 @@
-
 # ******* Generators in Python **********
 # ******* Generates values on fly, doesnt store in memory *******
 # ******* Can be traversed only once ******
-myGenerator = (x*2 for x in xrange(10))
+myGenerator = (x * 2 for x in xrange(10))
 
 for x in myGenerator:
     print(x)
@@ -14,15 +13,16 @@ for x in myGenerator:
 def createGenerator():
     mylist = xrange(5)
     for i in mylist:
-        yield i*i
+        yield i * i
+
+
 for x in createGenerator():
     print(x)
 
-
 # ***** Lists Append [Appends after last element of list] ******
-listNew = ['first object', 'second object']
-listNew.append('third_element')
-print (listNew[2])
+list_new = ['first object', 'second object']
+list_new.append('third_element')
+print (list_new[2])
 
 # ***** Lists Insert [Appends after last element of list] ******
 listNew = ['first_element', 'third_element']
@@ -30,20 +30,36 @@ listNew.insert(1, 'second_element')
 print (listNew[1])
 
 # ***** List sorting ******
-alist = [24, 12, 30, 5, 24]
-for x in sorted(alist, reverse=True):
+a_list = [24, 12, 30, 5, 24]
+for x in sorted(a_list, reverse=True):
     print x
 
 
 # ***** List sorting with key ******
 def find_maximum_number_of_a_chars(word):
-    repeatedA = 0
+    repeated_a_char = 0
     chars = list(word)
     for x in chars:
-        if x == 'a' or x == 'A':
-            repeatedA+1
-    return repeatedA
+        if x == 'a' or 'A' == x:
+            repeated_a_char += 1
+    return repeated_a_char
+
 
 word_list = ['Satyabrat', 'Ravishekher', 'Amit', 'Jonathan']
 for x in sorted(word_list, key=find_maximum_number_of_a_chars):
     print x
+
+# ***** Sort method, doesnt create a new list, its faster ******
+word_list.sort()
+# cant write sorted_list = word_list.sort()
+
+# Filter list
+# *** Filter numbers less than or equal to 10
+nums = [20, 8, 10, 6, 50, 60, 40, 14, 30]
+for x in [n for n in nums if n <= 10]:
+    print(x)
+
+# *** Select names containing 'y'
+names_list = ['Satyabrat', 'Ravishekher', 'Amit', 'Jonathan', 'XYZ']
+for name in [single_name for single_name in names_list if 'y' in single_name.lower()]:
+   print(name)
