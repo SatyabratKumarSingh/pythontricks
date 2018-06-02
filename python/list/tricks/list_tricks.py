faf01b6e3afb7ca953dfc7b1e4d2bd61a3fd89d8
satya <satyabratsingh@gmail.com>
2018-06-02 11:42:22 +0100

    Adding list tricks

--- python/list/tricks/list_tricks.py
+++ python/list/tricks/list_tricks.py
@@ -1,8 +1,30 @@
 
-##### Generators in Python #########
-##### Generates values on fly, doesnt store in memory #######
-##### Can be traversed only once #######
-myGenerator = (x*x for x in xrange(50))
+# ******* Generators in Python **********
+# ******* Generates values on fly, doesnt store in memory *******
+# ******* Can be traversed only once ******
+myGenerator = (x*2 for x in xrange(10))
 
 for x in myGenerator:
     print(x)
+
+
+# ****** Yields in Python *********
+# ***** Creates a generator *******
+# ***** Can be traversed only once ****
+def createGenerator():
+    mylist = xrange(5)
+    for i in mylist:
+        yield i*i
+for x in createGenerator():
+    print(x)
+
+
+# ***** Lists Append [Appends after last element of list] ******
+listNew = ['first object', 'second object']
+listNew.append('third_element')
+print (listNew[3])
+
+# ***** Lists Insert [Appends after last element of list] ******
+listNew = ['first_element', 'third_element']
+listNew.insert(1, 'second_element')
+print (listNew[1])
