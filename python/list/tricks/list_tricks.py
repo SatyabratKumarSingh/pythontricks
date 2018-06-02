38b8e7db988b1a9d847a0f3e66603f3b928063c7
satya <satyabratsingh@gmail.com>
2018-06-02 12:24:55 +0100

    Adding list tricks

--- python/list/tricks/list_tricks.py
+++ python/list/tricks/list_tricks.py
@@ -22,9 +22,28 @@ for x in createGenerator():
 # ***** Lists Append [Appends after last element of list] ******
 listNew = ['first object', 'second object']
 listNew.append('third_element')
-print (listNew[3])
+print (listNew[2])
 
 # ***** Lists Insert [Appends after last element of list] ******
 listNew = ['first_element', 'third_element']
 listNew.insert(1, 'second_element')
 print (listNew[1])
+
+# ***** List sorting ******
+alist = [24, 12, 30, 5, 24]
+for x in sorted(alist, reverse=True):
+    print x
+
+
+# ***** List sorting with key ******
+def find_maximum_number_of_a_chars(word):
+    repeatedA = 0
+    chars = list(word)
+    for x in chars:
+        if x == 'a' or x == 'A':
+            repeatedA+1
+    return repeatedA
+
+word_list = ['Satyabrat', 'Ravishekher', 'Amit', 'Jonathan']
+for x in sorted(word_list, key=find_maximum_number_of_a_chars):
+    print x
